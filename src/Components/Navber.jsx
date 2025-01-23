d7f46dfc00940e46a09c0c7807eb5768eadca46e
ibrahimkholilullah-web <nijmehalext@gmail.com>
2025-01-24 00:44:59 +0600

    all proje

--- src/Components/Navber.jsx
+++ src/Components/Navber.jsx
@@ -1,4 +1,3 @@
-'use client';
 import React from 'react';
 import { LoginLink, LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
